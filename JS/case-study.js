20a36da7f46b4b48507f106b845a5c11e4d8799d
ahmed16-eltabakh <aboali20202020123@gmail.com>
2025-06-15 17:17:24 +0300

    create the dots of the slider to the photos

--- JS/case-study.js
+++ JS/case-study.js
@@ -1,6 +1,7 @@
 let firstImg = document.querySelector(".first-img");
 let next = document.querySelector(".fa-circle-right");
 let prev = document.querySelector(".fa-circle-left");
+let dotsContainer = document.querySelector(".dots-container");
 let currentIndex = 0;
 
 let imgs = [
@@ -12,9 +13,27 @@ let imgs = [
     "../imgs/case-studies/case-study (6).jpg"
 ];
 
+imgs.forEach((_, index) =>{
+    let dot = document.createElement('span');
+    dot.classList.add("dot");
+
+    if(index === 0) dot.classList.add("active");
+    dot.addEventListener("click", ()=>{
+        currentIndex = index;
+        updateSlider();
+    });
+
+    dotsContainer.appendChild(dot);
+});
+
 
 function updateSlider(){
     firstImg.src = imgs[currentIndex];
+
+    let dots = document.querySelectorAll(".dot");
+    dots.forEach((dot, i)=>{
+        dot.classList.toggle('active', i === currentIndex);
+    });
 };
 
 next.addEventListener("click", ()=>{
