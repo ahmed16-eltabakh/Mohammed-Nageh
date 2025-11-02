803b08d26f3ed7aa6b13a811c017b17e74e410b3
ahmed16-eltabakh <aboali20202020123@gmail.com>
2025-11-02 18:49:17 +0200

    the faq section

--- JS/landing-page.js
+++ JS/landing-page.js
@@ -39,7 +39,13 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
+const faqItems = document.querySelectorAll('.faq-item');
 
+faqItems.forEach(item => {
+  item.querySelector('h3').addEventListener('click', () => {
+    item.classList.toggle('active');
+  });
+});
 
 
 
